62f8e6851db9f7455967cf08f02a6205632d4417
jeongsoyeon427 <jungsoyeon427@gmail.com>
2025-11-20 23:19:40 +0900

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/unitask-scheduler/",
   plugins: [react()],
+  base: "/unitask-scheduler/"
 });
